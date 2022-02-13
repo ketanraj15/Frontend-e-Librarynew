f3880de71fbc5416d844e18a9993560fd7cc5011
Ketan Raj <imh10015.18@bitmesra.ac.in>
2022-02-13 14:43:30 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://project-e-library.herokuapp.com/'});
+const API = axios.create({ baseURL: 'https://project-backend-e-library.herokuapp.com/'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
